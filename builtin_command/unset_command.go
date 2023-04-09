d2a417f526032ae583d33fac49cbf3fc5a134328
meiji <masaki@linkode.co.jp>
2023-04-09 22:03:16 +0900

    unset 不具合修正

--- builtin_command/unset_command.go
+++ builtin_command/unset_command.go
@@ -3,6 +3,7 @@ package builtin_command
 import (
 	"fmt"
 	"oreshell/ast"
+	"oreshell/log"
 	"oreshell/myvariables"
 )
 
@@ -10,11 +11,14 @@ var CommandNameUnset = "unset"
 
 // unsetコマンド
 func Unset(simpleCommand *ast.SimpleCommand) (err error) {
+	log.Logger.Printf("Unset start: %+v\n", simpleCommand)
+
 	args := simpleCommand.Args()
 	l := len(args)
+	log.Logger.Printf("Unset len(args): %+v\n", l)
 	if l == 0 {
 		return fmt.Errorf("%s: not enough arguments", CommandNameUnset)
-	} else if l > 1 {
+	} else if l >= 1 {
 		m := map[string]string{}
 		for _, v := range args {
 			m[v] = ""
